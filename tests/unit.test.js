fa31d16d30a0de7e18153178b62bca63f5876e16
Michal Kurek <mkurek@college.harvard.edu>
2025-04-05 17:52:50 -0400

    <-- THIS ACTION WILL FAIL. Bug introduced purposefully for this hw

--- tests/unit.test.js
+++ tests/unit.test.js
@@ -94,7 +94,7 @@ describe('Yale to Fale replacement logic', () => {
       return this.nodeType === 3;
     }).each(function () {
       const text = $(this).text();
-      const newText = text.replace(/Yale/g, 'Fale').replace(/yale/g, 'fale').replace(/YALE/g, 'FALE');
+      const newText = text.replace(/Yale/gi, 'Fale').replace(/yale/g, 'fale').replace(/YALE/g, 'FALE');
       if (text !== newText) {
         $(this).replaceWith(newText);
       }
